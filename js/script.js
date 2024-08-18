53c0b105869d6b9eece463325517694928dc570a
mahbt <mahsa.baghbani92@gmail.com>
2024-08-18 11:53:32 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* Toggle Mobile Menu */
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
+  const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
+  toggleButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+  });
+});
+/* Change Navbar background on scroll */
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar-scroll");
+  } else {
+    navbar.classList.remove("navbar-scroll");
+  }
+});
